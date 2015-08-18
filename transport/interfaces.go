89eb2b247692ae5a4016acec02d308666de85d36
Martin Hebnes Pedersen <martinhpedersen@users.noreply.github.com>
2015-08-18 19:34:56 +0200

    transport: Add Flusher, TxBuffer and Robust interfaces.
    These interfaces are currently only implemented by winmor, but
    provides some neat features that the current b2f implementation
    have not been able to utilize yet.

---
+++ transport/interfaces.go
@@ -0,0 +1,26 @@
+// Copyright 2015 Martin Hebnes Pedersen (LA5NTA). All rights reserved.
+// Use of this source code is governed by the MIT-license that can be
+// found in the LICENSE file.
+
+package transport
+
+type Flusher interface {
+	// Flush flushes the transmit buffers of the underlying modem.
+	Flush() error
+}
+
+type TxBuffer interface {
+	// TransmitBufferLen returns the number of bytes in the out buffer queue.
+	TxBufferLen() int
+}
+
+type Robust interface {
+	// Enables/disables robust mode.
+	SetRobust(r bool) error
+}
+
+// A BusyChannelChecker is a generic busy detector for a physical transmission medium.
+type BusyChannelChecker interface {
+	// Returns true if the channel is not clear
+	Busy() bool
+}

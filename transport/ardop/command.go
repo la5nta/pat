843a9d8dc0be3978e05f70297c48c9c156dfcdfc
Martin Hebnes Pedersen <martinhpedersen@users.noreply.github.com>
2015-10-08 10:54:24 +0200

    go fmt

--- transport/ardop/command.go
+++ transport/ardop/command.go
@@ -13,7 +13,7 @@ import (
 type Command string
 
 const (
-    //ardop
+	//ardop
 	cmdAbort           Command = "ABORT"           // Immediately aborts an ARQ Connection or a FEC Send session
 	cmdArqBW           Command = "ARQBW"           // <200MAX|500MAX|1000MAX|2000MAX|200FORCED|500FORCED|1000FORCED|2000FORCED>
 	cmdArqTimeout      Command = "ARQTIMEOUT"      // ARQTIMEOUT<30-240> Set/get the ARQ Timeout in seconds
@@ -26,22 +26,22 @@ const (
 	cmdCodec           Command = "CODEC"           // Start the Codec with True, Stop with False. No parameter will return the Codec state
 	cmdCWid            Command = "CWID"            // Disable/Enable the CWID option. CWID is optionally sent at the end of each ID frame.
 	cmdDataToSend      Command = "DATATOSEND"      // If sent with the parameter 0 (zero) it will clear the TNC’s data to send Queue. If sent without a parameter will return the current number of data to send bytes queued.
-    	cmdDebugLog        Command = "DEBUGLOG"        // Enable/disable the debug log
-    	cmdDisconnect      Command = "DISCONNECT"      // Initiates a normal disconnect cycle for an ARQ connection. If not connected command is ignored.
-    	cmdDisplay         Command = "DISPLAY"         // Sets the Dial frequency display of the Waterfall or Spectrum display. If sent without parameters will return the current Dial frequency display. If > 100000 Display will read in MHz.
-    	cmdDriveLevel      Command = "DRIVELEVEL"	   // Set Drive level. Default = 100 (max)
+	cmdDebugLog        Command = "DEBUGLOG"        // Enable/disable the debug log
+	cmdDisconnect      Command = "DISCONNECT"      // Initiates a normal disconnect cycle for an ARQ connection. If not connected command is ignored.
+	cmdDisplay         Command = "DISPLAY"         // Sets the Dial frequency display of the Waterfall or Spectrum display. If sent without parameters will return the current Dial frequency display. If > 100000 Display will read in MHz.
+	cmdDriveLevel      Command = "DRIVELEVEL"      // Set Drive level. Default = 100 (max)
 	cmdFECid           Command = "FECID"           // Disable/Enable ID (with optional grid square) at start of FEC transmissions
 	cmdFECmode         Command = "FECMODE"         // FECMODE<8FSK.200.25|4FSK.200.50S|4FSK.200.50,4PSK.200.100S|4PSK.200.100|8PSK.200.100|16FSK.500.25S|16FSK.500.25|4FSK.500.100S|4FSK.500.100| 4PSK.500.100|8PSK.500.100|4PSK.500.167|8PSK.500.167|4FSK.1000.100|4PSK.1000.100|8PSK.1000.100|4PSK.1000.167|8PSK.1000.167|4FSK.2000.600S|4FSK.2000.600|4FSK.2000.100|4PSK.2000.100|8PSK.2000.100|4PSK.2000.167|8PSK.2000.167
-    	cmdFECrepeats      Command = "FECREPEATS"      // <0-5> Sets the number of times a frame is repeated in FEC (multicast) mode. Higher number of repeats increases good copy probability under marginal conditions but reduces net throughput.
+	cmdFECrepeats      Command = "FECREPEATS"      // <0-5> Sets the number of times a frame is repeated in FEC (multicast) mode. Higher number of repeats increases good copy probability under marginal conditions but reduces net throughput.
 	cmdFECsend         Command = "FECSEND"         // Start/Stop FEC broadcast/multicast mode for specific FECMODE. FECSEND <False> will abort a FEC broadcast.
 	cmdGridSquare      Command = "GRIDSQUARE"      // <4, 6 or 8 character grid square>Sets or retrieves the 4, 6, or 8 character Maidenhead grid square (used in ID Frames) an improper grid square syntax will return a FAULT.
-    	cmdInitialize      Command = "INITIALIZE"      // Clears any pending queued values in the TNC interface. Should be sent upon initial connection and before any other parameters are sent
+	cmdInitialize      Command = "INITIALIZE"      // Clears any pending queued values in the TNC interface. Should be sent upon initial connection and before any other parameters are sent
 	cmdLeader          Command = "LEADER"          // LEADER<100-2000> Get/Set the leader length in ms. (Default is 160 ms). Rounded to the nearest 10 ms.
-    	cmdListen          Command = "LISTEN"          // Enables/disables server’s response to an ARQ connect request. Default = True. May be used to block connect requests during scanning.
+	cmdListen          Command = "LISTEN"          // Enables/disables server’s response to an ARQ connect request. Default = True. May be used to block connect requests during scanning.
 	cmdMyAux           Command = "MYAUX"           // <aux call sign1, aux call sign2, … aux call sign10>
 	cmdMyCall          Command = "MYCALL"          // Sets current call sign. If not a valid call generates a FAULT. Legitimate call signs include from 3 to 7 ASCII characters (A-Z, 0-9) followed by an optional “-“ and an SSID of -0 to -15 or -A to -Z. An SSID of -0 is treated as no SSID
 	cmdPlayback        Command = "PLAYBACK"        // <device name>Sets desired sound card playback device. If no device name will reply with the current assigned playback device.
-    	cmdPlaybackDevices Command = "PLAYBACKDEVICES" // Returns a comma delimited list of all currently installed playback devices.
+	cmdPlaybackDevices Command = "PLAYBACKDEVICES" // Returns a comma delimited list of all currently installed playback devices.
 	cmdProtocolMode    Command = "PROTOCOLMODE"    // PROTOCOLMODE<ARQ|FEC> Sets/Gets the protocol mode. If ARQ and LISTEN above is TRUE will answer Connect requests to MYCALL or any call signs in MYAUX. If FEC will decode but not respond to any connect request.
 	cmdRadioAnt        Command = "RADIOANT"        // Selects the radio antenna 1 or 2 for those radios that support antenna switching. If the parameter is 0 will not change the antenna setting even if the radio supports it. If sent without a parameter will return 0, 1 or 2. If RADIOCONTROL Is false or RADIOMODEL has not been set will return FAULT
 	cmdRadioCtrl       Command = "RADIOCTRL"       // Enables/disables the radio control capability of the ARDOP_Win TNC. If sent without a parameter will return the current value of RADIOCONTROL enable.
@@ -52,31 +52,31 @@ const (
 	cmdRadioFilter     Command = "RADIOFILTER"     //
 	cmdRadioFreq       Command = "RADIOFREQ"       //
 	cmdRadioComAdd     Command = "RADIOCOMADD"     // 00-FF> Sets/reads the current Icom Address for radio control (Icom radios only). Values must be hex 00 through FF
-    	cmdRadioISC        Command = "RADIOISC"        // Enable/Disable Radio’s internal sound card (some radios)
+	cmdRadioISC        Command = "RADIOISC"        // Enable/Disable Radio’s internal sound card (some radios)
 	cmdRadioMenu       Command = "RADIOMENU"
-    	cmdRadioMode       Command = "RADIOMODE"       // USB,USBD, FM>
+	cmdRadioMode       Command = "RADIOMODE" // USB,USBD, FM>
 	cmdRadioModel      Command = "RADIOMODEL"
 	cmdRadioModels     Command = "RADIOMODELS"
-	cmdRadioPTT        Command = "RADIOPTT"        // CATPTT|VOX/SIGNALINK|COMn
+	cmdRadioPTT        Command = "RADIOPTT" // CATPTT|VOX/SIGNALINK|COMn
 	cmdRadioPTTDTR     Command = "RADIOPTTDTR"
 	cmdRadioPTTRTS     Command = "RADIOPTTRTS"
 	// end of radio commands
-	
-	cmdSendID          Command = "SENDID"
-	cmdSetupMenu       Command = "SETUPMENU"
-	cmdSquelch         Command = "SQUELCH"
-	cmdState           Command = "STATE"
-	cmdTrailer         Command = "TRAILER"
-	cmdTuneRange       Command = "TUNERANGE"
-	cmdTwoToneTest     Command = "TWOTONETEST"
-	cmdVersion         Command = "VERSION"
+
+	cmdSendID      Command = "SENDID"
+	cmdSetupMenu   Command = "SETUPMENU"
+	cmdSquelch     Command = "SQUELCH"
+	cmdState       Command = "STATE"
+	cmdTrailer     Command = "TRAILER"
+	cmdTuneRange   Command = "TUNERANGE"
+	cmdTwoToneTest Command = "TWOTONETEST"
+	cmdVersion     Command = "VERSION"
 
 	// ardop not implemented
-	cmdAutoBreak       Command = "AUTOBREAK"
-	cmdBreak           Command = "BREAK"
-	cmdBusyLock        Command = "BUSYLOCK"
-	cmdRadioTuner      Command = "RADIOTUNER"
-	
+	cmdAutoBreak  Command = "AUTOBREAK"
+	cmdBreak      Command = "BREAK"
+	cmdBusyLock   Command = "BUSYLOCK"
+	cmdRadioTuner Command = "RADIOTUNER"
+
 	//
 	//cmdRobust          Command = "ROBUST"          // <>[bool]: Force the most robust mode (2x4FSK) for the entire session
 	//cmdPrompt          Command = "CMD"             // <[]: Seems like a command prompt

a5937ba2646a443e34a37ff3cb79ae022f622b53
Martin Hebnes Pedersen <martinhpedersen@users.noreply.github.com>
2015-11-01 21:56:16 +0100

    cmd/wl2k: Fix make.bash with Go < 1.5

--- make.bash
+++ make.bash
@@ -2,7 +2,14 @@
 set -e
 
 GITREV=$(git rev-parse --short HEAD)
+GO_POINT_VERSION=$(go version|awk 'match($0, /1\.[0-9]+/) { print substr( $0, RSTART+2, RLENGTH-2 )}')
 
 set -x
-go test -tags "$TAGS" ../...
-go build -v -tags "$TAGS" -ldflags "-X \"main.GitRev=$GITREV\""
+
+go test -tags "$TAGS" ../../...
+
+if [ "$GO_POINT_VERSION" -gt "4" ]; then
+	go build -v -tags "$TAGS" -ldflags "-X \"main.GitRev=$GITREV\""
+else
+	go build -v -tags "$TAGS" -ldflags "-X \"main.GitRev $GITREV\""
+fi

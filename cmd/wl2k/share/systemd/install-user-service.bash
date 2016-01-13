5072348694e711d64565691d5e7f357a8c0d8abc
Martin Hebnes Pedersen <martinhpedersen@users.noreply.github.com>
2016-01-14 00:40:21 +0100

    wl2k: Fix systemd user service install script.

--- cmd/wl2k/share/systemd/install-user-service.bash
+++ cmd/wl2k/share/systemd/install-user-service.bash
@@ -3,6 +3,7 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+mkdir -p "$HOME/.config/systemd/user"
 cp "$DIR/wl2k.service" "$HOME/.config/systemd/user/"
 systemctl --user daemon-reload
 

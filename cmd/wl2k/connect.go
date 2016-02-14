la5nta/pat

commit 23fc548d2d0cc9d686ddea10e9062823a98a0331

wl2k: Fix digipeater syntax for ax25 and serial-tnc
Closes #28

cmd/wl2k/connect.go
@@ -10,6 +10,7 @@ import (
 	"net"
 	"net/url"
 	"path"
+	"sort"
 	"strings"
 	"time"
 
@@ -42,11 +43,18 @@ func Connect(connectStr string) (success bool) {
 		return false
 	}
 
-	targetcall := path.Base(url.Path)
+	via, targetcall := path.Split(url.Path)
 
 	if len(targetcall) < 3 {
 		log.Println("Missing targetcall in connection URL")
 		return false
+	}
+
+	// Prepare a slice of digipeaters for use with AX.25
+	digis := strings.Split(strings.Trim(via, "/"), "/")
+	sort.Reverse(sort.StringSlice(digis))
+	if len(digis) > 0 {
+		targetcall = fmt.Sprintf("%s via %s", targetcall, strings.Join(digis, " "))
 	}
 
 	// QSY

commit 4c195c1eaccd737e1276243a244c1dbd0e6d4ef5

Fix winmor/ardop connect 'via' issue
This bug was introduced in 23fc548d2d0cc9d686ddea10e9062823a98a0331

cmd/wl2k/connect.go
@@ -53,7 +53,7 @@ func Connect(connectStr string) (success bool) {
 	// Prepare a slice of digipeaters for use with AX.25
 	digis := strings.Split(strings.Trim(via, "/"), "/")
 	sort.Reverse(sort.StringSlice(digis))
-	if len(digis) > 0 {
+	if len(digis) > 0 && digis[0] != "" {
 		targetcall = fmt.Sprintf("%s via %s", targetcall, strings.Join(digis, " "))
 	}
 
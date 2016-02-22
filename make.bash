f5568be99f795e5579cb51cbba42942ef27f36e2
Martin Hebnes Pedersen <martinhpedersen@users.noreply.github.com>
2016-02-22 21:54:32 +0100

    Simplify make.bash and test wl2k-go

--- make.bash
+++ make.bash
@@ -2,14 +2,13 @@
 set -e
 
 GITREV=$(git rev-parse --short HEAD)
+GO15VENDOREXPERIMENT=1
+
+# Go 1.5 or later is required
 GO_POINT_VERSION=$(go version| perl -ne 'm/go1\.(\d)/; print $1;')
+[ "$GO_POINT_VERSION" -lt "5" ] && echo "Go 1.5 or later required" && exit 1;
 
 set -x
 
-go test -tags "$TAGS" ../../...
-
-if [ "$GO_POINT_VERSION" -gt "4" ]; then
-	go build -v -tags "$TAGS" -ldflags "-X \"main.GitRev=$GITREV\""
-else
-	go build -v -tags "$TAGS" -ldflags "-X main.GitRev \"$GITREV\""
-fi
+go test -tags "$TAGS" `go list ./...|grep -v vendor` `go list ./...|grep wl2k-go|egrep -v '/vendor/.*/vendor/'`
+go build -v -tags "$TAGS" -ldflags "-X \"main.GitRev=$GITREV\""

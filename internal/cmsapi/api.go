b9742b7461ffb9c91a8ebb0fc57fc0f54039c7d8
Martin Hebnes Pedersen <martinhpedersen@users.noreply.github.com>
2016-03-08 22:40:16 +0100

    Add missing copyright notice

--- internal/cmsapi/api.go
+++ internal/cmsapi/api.go
@@ -1,3 +1,7 @@
+// Copyright 2016 Martin Hebnes Pedersen (LA5NTA). All rights reserved.
+// Use of this source code is governed by the MIT-license that can be
+// found in the LICENSE file.
+
 package cmsapi
 
 import (

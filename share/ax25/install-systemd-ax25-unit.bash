d622efa257bf901f950dbe9b8c606c2ee2aba956
Martin Hebnes Pedersen <martinhpedersen@users.noreply.github.com>
2016-08-01 16:45:20 +0200

    ax25: Move systemd unit file to /lib/systemd/system

--- share/ax25/install-systemd-ax25-unit.bash
+++ share/ax25/install-systemd-ax25-unit.bash
@@ -3,8 +3,9 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cp "$DIR/ax25.service" "/etc/systemd/system/ax25.service"
-cp "$DIR/ax25.default" "/etc/default/ax25"
+[ -e "/etc/systemd/system/ax25.service" ] && rm /etc/systemd/system/ax25.service
+cp "$DIR/ax25.service" "/lib/systemd/system/ax25.service"
+[ -e "/etc/default/ax25" ] || cp "$DIR/ax25.default" "/etc/default/ax25"
 systemctl daemon-reload
 
 echo "Installed. Edit /etc/default/ax25 and start with 'systemctl start ax25'"

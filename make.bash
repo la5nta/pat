cdad92f6cf7a468b608b7f54cbcc4c23fc701ca6
Martin Hebnes Pedersen <martin.h.pedersen@gmail.com>
2016-09-12 18:23:16 +0200

    make.bash: Build pkg on darwin if tool is available

--- make.bash
+++ make.bash
@@ -4,6 +4,7 @@ set -e
 export GO15VENDOREXPERIMENT=1
 
 GITREV=$(git rev-parse --short HEAD)
+VERSION=$(grep Version version.go|cut -d '"' -f2)
 
 # Go 1.5 or later is required
 GO_POINT_VERSION=$(go version| perl -ne 'm/go1\.(\d)/; print $1;')
@@ -16,3 +17,9 @@ git submodule update --init --recursive
 
 go test -tags "$TAGS" `go list ./...|grep -v vendor` `go list ./...|grep wl2k-go|egrep -v '/vendor/.*/vendor/'`
 go build -v -tags "$TAGS" -ldflags "-X \"main.GitRev=$GITREV\""
+
+# Build macOS pkg (amd64)
+if [[ "$OSTYPE" == "darwin"* ]] && command -v packagesbuild >/dev/null 2>&1; then
+	packagesbuild osx/pat.pkgproj
+	mv 'Pat :: A Modern Winlink Client.pkg' "pat_${VERSION}_darwin_amd64_unsigned.pkg"
+fi

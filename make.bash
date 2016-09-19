d611b270e3039c94b558cd593922f4485f74f9a9
Martin Hebnes Pedersen <martin.h.pedersen@gmail.com>
2016-09-19 20:03:17 +0200

    make.bash: Enable libax25 on Linux by default

--- make.bash
+++ make.bash
@@ -10,16 +10,31 @@ VERSION=$(grep Version VERSION.go|cut -d '"' -f2)
 GO_POINT_VERSION=$(go version| perl -ne 'm/go1\.(\d)/; print $1;')
 [ "$GO_POINT_VERSION" -lt "5" ] && echo "Go 1.5 or later required" && exit 1;
 
-set -x
+# Link against libax25 on Linux
+if [[ "$OSTYPE" == "linux"* ]]; then
+	if [[ -f "/usr/lib/libax25.a" ]]; then
+		TAGS="libax25"
+	else
+		echo "WARNING: Not linking with libax25 - /usr/lib/libax25.a not found."
+	fi
+fi
 
 # Update submodules (dependencies)
+echo -e "Updating git submodules..."
 git submodule update --init --recursive
 
+echo "Running tests..."
 go test -tags "$TAGS" `go list ./...|grep -v vendor` `go list ./...|grep wl2k-go|egrep -v '/vendor/.*/vendor/'`
-go build -v -tags "$TAGS" -ldflags "-X \"main.GitRev=$GITREV\""
+echo
+
+echo "Building Pat v$VERSION..."
+go build -tags "$TAGS" -ldflags "-X \"main.GitRev=$GITREV\""
 
 # Build macOS pkg (amd64)
 if [[ "$OSTYPE" == "darwin"* ]] && command -v packagesbuild >/dev/null 2>&1; then
+	echo "Generating macOS installer package..."
 	packagesbuild osx/pat.pkgproj
 	mv 'Pat :: A Modern Winlink Client.pkg' "pat_${VERSION}_darwin_amd64_unsigned.pkg"
 fi
+
+echo -e "Enjoy!"

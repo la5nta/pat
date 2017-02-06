1e3fa426b6524af739d84c64b6c1e3be25871121
Martin Hebnes Pedersen <martin.h.pedersen@gmail.com>
2017-02-06 17:44:10 +0100

    Happy new year

--- VERSION.go
+++ VERSION.go
@@ -1,4 +1,4 @@
-// Copyright 2016 Martin Hebnes Pedersen (LA5NTA). All rights reserved.
+// Copyright 2017 Martin Hebnes Pedersen (LA5NTA). All rights reserved.
 // Use of this source code is governed by the MIT-license that can be
 // found in the LICENSE file.
 

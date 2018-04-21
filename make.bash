8ddb42c68643577c140f08563ba548a87280560d
Martin Hebnes Pedersen <martin.h.pedersen@gmail.com>
2018-04-21 11:51:17 +0200

    Update libax25 dist URL for build script
    www.linux-ax25.org is currently down for unknown reasons. Use a debian
    mirror, which should be more stable.

--- make.bash
+++ make.bash
@@ -12,11 +12,13 @@ VERSION=$(grep Version VERSION.go|cut -d '"' -f2)
 GO_POINT_VERSION=$(go version| perl -ne 'm/go1\.(\d+)/; print $1;')
 [ "$GO_POINT_VERSION" -lt "5" ] && echo "Go 1.5 or later required" && exit 1;
 
-AX25DIST="libax25-0.0.12-rc4"
+AX25VERSION="0.0.12-rc4"
+AX25DIST="libax25-${AX25VERSION}"
+AX25DIST_URL="http://http.debian.net/debian/pool/main/liba/libax25/libax25_${AX25VERSION}.orig.tar.gz"
 function install_libax25 {
 	mkdir -p .build && cd .build
-	[[ -f "${AX25DIST}.tar.gz" ]] || curl -Ssf "http://www.linux-ax25.org/pub/libax25/${AX25DIST}.tar.gz" | tar zx
-	cd "${AX25DIST}" && ./configure --prefix=/ && make && cd ../../
+	[[ -f "${AX25DIST}" ]] || curl -LSsf "${AX25DIST_URL}" | tar zx
+	cd "${AX25DIST}/" && ./configure --prefix=/ && make && cd ../../
 }
 
 [[ "$1" == "libax25" ]] && install_libax25 && exit 0;

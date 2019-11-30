e037509d7b03fd80d1dbbb8440a86c817bac89bd
Chris Keller <xylo04@gmail.com>
2019-11-29 19:47:58 -0700

    Add sorting to message table headers
    Fixes #154

--- res/js/index.js
+++ res/js/index.js
@@ -609,6 +609,12 @@ function updateConsole(msg)
 	pre.scrollTop( pre.prop("scrollHeight") );
 }
 
+const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+
+const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
+		v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+)(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+
 var currentFolder;
 function displayFolder(dir) {
 	currentFolder = dir;
@@ -655,6 +661,19 @@ function displayFolder(dir) {
 			});
 		}
 	});
+	// Adapted from https://stackoverflow.com/a/49041392
+	document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+		const table = th.closest('table');
+		const tbody = table.querySelector('tbody');
+		Array.from(tbody.querySelectorAll('tr'))
+			.sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+			.forEach(tr => tbody.appendChild(tr));
+		const previousTh = table.querySelector('th.sorted');
+		if (previousTh != null) {
+			previousTh.classList.remove('sorted');
+		}
+		th.classList.add('sorted');
+	})));
 }
 
 function displayMessage(elem) {

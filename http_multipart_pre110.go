1b4965758d143aa3ce867766df784837b94c7b74
Martin Hebnes Pedersen <martin.h.pedersen@gmail.com>
2019-12-08 23:58:50 +0100

    Require Go 1.10 or later
    Debian stable now ships with Go 1.11, and 1.11 are available for
    oldstable through backports. Keeping compatibility with older versions
    seems unnecessary.
    
    Users are adviced to use the latest stable Go version.
    
    Closes #122

--- http_multipart_pre110.go
+++
@@ -1,8 +0,0 @@
-// +build !go1.10
-
-package main
-
-import "mime/multipart"
-
-// Prior to Go 1.10, empty form files was filtered automatically by the multipart.Parser
-func isEmptyFormFile(f *multipart.FileHeader) bool { return false }

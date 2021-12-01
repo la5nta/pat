1ba06731db121be37dfeac058b2b401ab20a5114
Martin Hebnes Pedersen <martin.h.pedersen@gmail.com>
2021-12-01 19:42:39 +0100

    Cleanup and improve error handling of forms unzip

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -344,56 +344,50 @@ func (m *Manager) downloadAndUnzipForms(downloadLink string) error {
 	return nil
 }
 
-func unzip(src, dest string) error {
-	// https://stackoverflow.com/a/24792688/587091
-	r, err := zip.OpenReader(src)
+func unzip(srcArchivePath, dstRoot string) error {
+	// Closure to address file descriptors issue with all the deferred .Close() methods
+	extractAndWriteFile := func(zf *zip.File) error {
+		if zf.FileInfo().IsDir() {
+			return nil
+		}
+		destPath := filepath.Join(dstRoot, zf.Name)
+
+		// Check for ZipSlip (Directory traversal)
+		if !strings.HasPrefix(destPath, filepath.Clean(dstRoot)+string(os.PathSeparator)) {
+			return fmt.Errorf("illegal file path: %s", destPath)
+		}
+
+		// Ensure target directory exists
+		if err := os.MkdirAll(filepath.Dir(destPath), 0755); err != nil {
+			return fmt.Errorf("can't create target directory: %w", err)
+		}
+
+		// Write file
+		src, err := zf.Open()
+		if err != nil {
+			return err
+		}
+		defer src.Close()
+		dst, err := os.OpenFile(destPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, zf.Mode())
+		if err != nil {
+			return err
+		}
+		defer dst.Close()
+		_, err = io.Copy(dst, src)
+		return err
+	}
+
+	r, err := zip.OpenReader(srcArchivePath)
 	if err != nil {
 		return err
 	}
 	defer r.Close()
-
-	_ = os.MkdirAll(dest, 0o755)
-
-	// Closure to address file descriptors issue with all the deferred .Close() methods
-	extractAndWriteFile := func(f *zip.File) error {
-		rc, err := f.Open()
-		if err != nil {
-			return err
-		}
-		defer rc.Close()
-
-		p := filepath.Join(dest, f.Name)
-
-		// Check for ZipSlip (Directory traversal)
-		if !strings.HasPrefix(p, filepath.Clean(dest)+string(os.PathSeparator)) {
-			return fmt.Errorf("illegal file path: %s", p)
-		}
-
-		if f.FileInfo().IsDir() {
-			_ = os.MkdirAll(p, 0755)
-		} else {
-			_ = os.MkdirAll(filepath.Dir(p), 0755)
-			f, err := os.OpenFile(p, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-			if err != nil {
-				return err
-			}
-			defer f.Close()
-
-			_, err = io.Copy(f, rc)
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	}
-
 	for _, f := range r.File {
 		err := extractAndWriteFile(f)
 		if err != nil {
 			return err
 		}
 	}
-
 	return nil
 }
 

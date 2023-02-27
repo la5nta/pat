1e738c71baeecd12472b081dccf296f43dc7cc5a
ThreeSixes <joshlucy@gmail.com>
2023-02-26 20:40:02 -0800

    No more entrypoint.

--- docker/assets/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-/app/pat --config /app/config.json \
-    --event-log /app/logs/eventlog.json \
-    --log /app/logs/pat.log \
-    --mbox /app/mailbox \
-    --forms /app/standard_forms \
-    --mycall ${MYCALL} http

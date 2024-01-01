5b966989ed9bf4ebd001c1ae1ac6b7dd654eebb2
Martin Hebnes Pedersen <martin.h.pedersen@gmail.com>
2024-01-01 08:53:45 +0100

    Distinguish Template from Form
    (HTML) Forms is an extension of the Templates feature in RMS Express.
    Templates are the .txt files. Some templates support HTML forms,
    indicated by the Template Control Field `Form`.

--- cli_composer.go
+++ cli_composer.go
@@ -283,7 +283,7 @@ func composeFormReport(ctx context.Context, args []string) {
 
 	msg := composeMessageHeader(nil)
 
-	formMsg, err := formsMgr.ComposeForm(tmplPathArg, msg.Subject())
+	formMsg, err := formsMgr.ComposeTemplate(tmplPathArg, msg.Subject())
 	if err != nil {
 		log.Printf("failed to compose message for template: %v", err)
 		return

a0076fd6a9ef7fadf5302ebee2d6e6cb693249f3
Martin Hebnes Pedersen <martin.h.pedersen@gmail.com>
2024-01-06 13:59:51 +0100

    Refactor XML marshalling and add tests
    Also fixed issue of inconsistent output due to unstable ordering of form
    variables.

--- internal/forms/builder_test.go
+++ internal/forms/builder_test.go
@@ -1,6 +1,8 @@
 package forms
 
 import (
+	"bufio"
+	"bytes"
 	"testing"
 	"time"
 
@@ -47,4 +49,65 @@ func TestInsertionTagReplacer(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestBuildXML(t *testing.T) {
+	location = time.FixedZone("UTC+1", 1*60*60)
+	now = func() time.Time { return time.Date(1988, 3, 21, 00, 00, 00, 00, location).In(time.UTC) }
+	b := messageBuilder{
+		FormsMgr: &Manager{config: Config{
+			MyCall:     "LA5NTA",
+			AppVersion: "v1.0.0",
+			Locator:    "JO29PJ",
+			GPSd:       cfg.GPSdConfig{Addr: gpsMockAddr},
+		}},
+		Template: Template{ViewerURI: "viewer.html", ReplyTxtFileURI: "reply.txt"},
+		FormValues: map[string]string{
+			"var1": "foo",
+			"var2": "bar",
+		},
+	}
+	expect := []byte(`
+        <?xml version="1.0" encoding="UTF-8"?>
+        <RMS_Express_Form>
+          <form_parameters>
+            <xml_file_version>1.0</xml_file_version>
+            <rms_express_version>v1.0.0</rms_express_version>
+            <submission_datetime>19880320230000</submission_datetime>
+            <senders_callsign>LA5NTA</senders_callsign>
+            <grid_square>JO29PJ</grid_square>
+            <display_form>viewer.html</display_form>
+            <reply_template>reply.txt</reply_template>
+          </form_parameters>
+          <variables>
+            <var1>foo</var1>
+            <var2>bar</var2>
+          </variables>
+        </RMS_Express_Form>
+	`)
+	if got := b.buildXML(); !xmlEqual(t, got, expect) {
+		t.Errorf("Got unexpected XML:\n%s\n", string(got))
+	}
+}
+
+// xmlEqual compares two byte slices line by line, ignoring leading/trailing whitespace.
+func xmlEqual(t *testing.T, a, b []byte) bool {
+	lines := func(xml []byte) (slice [][]byte) {
+		s := bufio.NewScanner(bytes.NewReader(xml))
+		for s.Scan() {
+			l := bytes.TrimSpace(s.Bytes())
+			if len(l) > 0 {
+				slice = append(slice, l)
+			}
+		}
+		return slice
+	}
+	aLines, bLines := lines(a), lines(b)
+	for i := 0; i < len(aLines) && i < len(bLines); i++ {
+		if !bytes.Equal(aLines[i], bLines[i]) {
+			t.Errorf("%q != %q", aLines[i], bLines[i])
+			return false
+		}
+	}
+	return len(aLines) == len(bLines)
 }

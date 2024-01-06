bc9512e159b4f1fc745830a3bb3e37f0e6a52ac5
Martin Hebnes Pedersen <martin.h.pedersen@gmail.com>
2024-01-06 09:37:31 +0100

    Add a couple of missing insertion Tags + docs
    <Day>, <UDay>, <GPSLatitude> and <GPSLongitude>

--- internal/forms/date.go
+++ internal/forms/date.go
@@ -19,3 +19,5 @@ func formatTime(t time.Time) string        { return t.In(location).Format("15:04
 func formatDateUTC(t time.Time) string     { return t.UTC().Format("2006-01-02Z07:00") }
 func formatTimeUTC(t time.Time) string     { return t.UTC().Format("15:04:05Z07:00") }
 func formatUDTG(t time.Time) string        { return strings.ToUpper(t.UTC().Format("021504Z07:00 Jan 2006")) }
+
+func formatDay(t time.Time, in *time.Location) string { return t.In(in).Weekday().String() }

35988914fb74f96f0ec139bea9b516a7201b25ed
Martin Hebnes Pedersen <martin.h.pedersen@gmail.com>
2024-01-06 09:13:36 +0100

    Add more insertion tags tests

--- internal/forms/builder_test.go
+++ internal/forms/builder_test.go
@@ -2,22 +2,45 @@ package forms
 
 import (
 	"testing"
+	"time"
 
 	"github.com/la5nta/pat/cfg"
 )
 
 func TestInsertionTagReplacer(t *testing.T) {
 	m := &Manager{config: Config{
-		MyCall: "LA5NTA",
-		GPSd:   cfg.GPSdConfig{Addr: gpsMockAddr},
+		MyCall:     "LA5NTA",
+		AppVersion: "v1.0.0",
+		GPSd:       cfg.GPSdConfig{Addr: gpsMockAddr},
 	}}
+	location = time.FixedZone("UTC+1", 1*60*60)
+	now = func() time.Time { return time.Date(1988, 3, 21, 00, 00, 00, 00, location).In(time.UTC) }
 	tests := map[string]string{
-		"<GridSquare>": "JO29PJ",
+		"<ProgramVersion>": "Pat v1.0.0",
+		"<Callsign>":       "LA5NTA",
+		"<MsgSender>":      "LA5NTA",
+
+		"<DateTime>":  "1988-03-21 00:00:00",
+		"<UDateTime>": "1988-03-20 23:00:00Z",
+		"<Date>":      "1988-03-21",
+		"<UDate>":     "1988-03-20Z",
+		"<UDTG>":      "202300Z MAR 1988",
+		"<Time>":      "00:00:00",
+		"<UTime>":     "23:00:00Z",
+
+		"<GPS>":                "59-24.83N 005-16.08E",
+		"<GPS_DECIMAL>":        "59.4138N 5.2680E",
+		"<GPS_SIGNED_DECIMAL>": "59.4138 5.2680",
+		"<GridSquare>":         "JO29PJ",
+		"<Latitude>":           "59.4138",
+		"<Longitude>":          "5.2680",
+		"<GPSValid>":           "YES ", // This trailing space appears to be intentional,
 	}
 	for in, expect := range tests {
-		if out := insertionTagReplacer(m, "<", ">")(in); out != expect {
-			t.Errorf("%s: Expected %q, got %q", in, expect, out)
-		}
-
+		t.Run(in, func(t *testing.T) {
+			if out := insertionTagReplacer(m, "<", ">")(in); out != expect {
+				t.Errorf("Expected %q, got %q", expect, out)
+			}
+		})
 	}
 }

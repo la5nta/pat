2df1a56b8486b6ebd7f37d8636a6a79c235ced90
Martin Hebnes Pedersen <martin.h.pedersen@gmail.com>
2024-01-30 20:12:28 +0100

    Fallback to global file reference search
    Some templates references HTML Forms and reply templates located in
    adjecent folders, without specifying a relative path. Examples of this
    can be found in `MAPPING-GIS FORMS`.

--- internal/forms/template.go
+++ internal/forms/template.go
@@ -2,6 +2,8 @@ package forms
 
 import (
 	"bufio"
+	"io/fs"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -21,7 +23,7 @@ type Template struct {
 	ReplyViewerURI  string `json:"reply_viewer_uri"`
 }
 
-func readTemplate(path string) (Template, error) {
+func readTemplate(path string, filesMap formFilesMap) (Template, error) {
 	f, err := os.Open(path)
 	if err != nil {
 		return Template{}, err
@@ -33,21 +35,20 @@ func readTemplate(path string) (Template, error) {
 		TxtFileURI: path,
 	}
 	scanner := bufio.NewScanner(f)
-	baseURI := filepath.Dir(template.TxtFileURI)
 	for scanner.Scan() {
 		switch key, value, _ := strings.Cut(scanner.Text(), ":"); key {
 		case "Form":
 			// Form: <composer>,<viewer>
 			files := strings.Split(value, ",")
 			// Extend to absolute paths and add missing html extension
-			for i, path := range files {
-				path = strings.TrimSpace(path)
-				if ext := filepath.Ext(path); ext == "" {
-					path += htmlFileExt
+			for i, name := range files {
+				name = strings.TrimSpace(name)
+				if ext := filepath.Ext(name); ext == "" {
+					name += htmlFileExt
 				}
-				files[i] = resolveFileReference(baseURI, path)
+				files[i] = resolveFileReference(filesMap, filepath.Dir(path), name)
 				if files[i] == "" {
-					debug.Printf("%s: failed to resolve referenced file %q", template.TxtFileURI, path)
+					debug.Printf("%s: failed to resolve referenced file %q", template.TxtFileURI, name)
 				}
 			}
 			template.InitialURI = files[0]
@@ -55,17 +56,17 @@ func readTemplate(path string) (Template, error) {
 				template.ViewerURI = files[1]
 			}
 		case "ReplyTemplate":
-			path := strings.TrimSpace(value)
-			// Some are missing .txt
-			if filepath.Ext(path) == "" {
-				path += txtFileExt
+			name := strings.TrimSpace(value)
+			// Some are missing file extension (default to .txt)
+			if filepath.Ext(name) == "" {
+				name += txtFileExt
 			}
-			path = resolveFileReference(baseURI, path)
+			path := resolveFileReference(filesMap, filepath.Dir(path), name)
 			if path == "" {
-				debug.Printf("%s: failed to resolve referenced reply template file %q", template.TxtFileURI, path)
+				debug.Printf("%s: failed to resolve referenced reply template file %q", template.TxtFileURI, name)
 				continue
 			}
-			replyTemplate, err := readTemplate(path)
+			replyTemplate, err := readTemplate(path, filesMap)
 			if err != nil {
 				debug.Printf("%s: failed to load referenced reply template: %v", template.TxtFileURI, err)
 			}
@@ -75,6 +76,22 @@ func readTemplate(path string) (Template, error) {
 		}
 	}
 	return template, err
+}
+
+// resolveFileReference searches for files referenced in .txt files.
+//
+// If found the returned path is the absolute path, otherwise an empty string is returned.
+func resolveFileReference(filesMap formFilesMap, basePath string, referencePath string) string {
+	path := filepath.Join(basePath, referencePath)
+	if !directories.IsInPath(basePath, path) {
+		debug.Printf("%q escapes template's base path (%q)", referencePath, basePath)
+		return ""
+	}
+	if _, err := os.Stat(path); err == nil {
+		return path
+	}
+	debug.Printf("fallback to map based lookup of %q", filepath.Join(filepath.Base(basePath), referencePath))
+	return filesMap.get(referencePath)
 }
 
 func (t Template) matchesName(nameToMatch string) bool {
@@ -99,31 +116,54 @@ func (t Template) containsName(partialName string) bool {
 		strings.Contains(t.TxtFileURI, partialName)
 }
 
-// resolveFileReference searches for files referenced in .txt files.
-//
-// If found the returned path is the absolute path, otherwise an empty string is returned.
-func resolveFileReference(basePath string, referencePath string) string {
-	path := filepath.Join(basePath, referencePath)
-	if !directories.IsInPath(basePath, path) {
-		debug.Printf("%q escapes template's base path (%q)", referencePath, basePath)
-		return ""
-	}
-	if _, err := os.Stat(path); err == nil {
+type formFilesMap map[string]string
+
+func (m formFilesMap) get(name string) string {
+	name = strings.ToLower(name)
+	if path, ok := m[name]; ok {
 		return path
-	}
-	// Fallback to case-insenstive search.
-	// Some HTML files references in the .txt files has a different caseness than the actual filename on disk.
-	//TODO: Walk basePath tree instead
-	absPathTemplateFolder := filepath.Dir(path)
-	entries, err := os.ReadDir(absPathTemplateFolder)
-	if err != nil {
-		return path
-	}
-	for _, entry := range entries {
-		name := entry.Name()
-		if strings.EqualFold(filepath.Base(path), name) {
-			return filepath.Join(absPathTemplateFolder, name)
-		}
 	}
 	return ""
+}
+
+// formFilesFromPath returns a map from filenames to absolute paths of
+// identified HTML Forms and reply templates within the given base path.
+func formFilesFromPath(basePath string) formFilesMap {
+	m := make(map[string]string)
+	isFormFile := func(name string) bool {
+		return strings.EqualFold(filepath.Ext(name), htmlFileExt)
+	}
+	isReplyTemplate := func(name string) bool {
+		return strings.EqualFold(filepath.Ext(name), ".0")
+	}
+	add := func(name, path string) {
+		name = strings.ToLower(name)
+		if !(isFormFile(name) || isReplyTemplate(name)) {
+			return
+		}
+		if dup, ok := m[name]; ok {
+			debug.Printf("duplicate filenames: %q, %q", path, dup)
+		}
+		m[name] = path
+	}
+	err := fs.WalkDir(os.DirFS(basePath), ".", func(path string, d fs.DirEntry, err error) error {
+		switch {
+		case path == ".":
+			return nil
+		case err != nil:
+			return err
+		case d.IsDir():
+			for k, v := range formFilesFromPath(path) {
+				add(k, v)
+			}
+			return nil
+		default:
+			add(d.Name(), filepath.Join(basePath, path))
+			return nil
+		}
+	})
+	if err != nil {
+		log.Printf("failed to walk path %q: %v", basePath, err)
+	}
+	return m
 }

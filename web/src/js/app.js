la5nta/pat

commit 7d43a6d534955807ed8dc243998b40f6acd6ffb2

Preservative refresh of Connect URI on input
To preserve unfamiliar parameters from aliases etc when a form field is
used to tweak the connection parameters.

web/src/js/app.js
@@ -591,26 +591,31 @@ function getConnectURL() { return $('#connectURLInput').val(); }
 function setConnectURL(url) { $('#connectURLInput').val(url); }
 
 function buildConnectURL() {
-  let url =
-    $('#transportSelect').val() + '://' + $('#addrInput').val() + '/' + $('#targetInput').val();
-
-  let params = '';
-
+  // Instead of building from scratch, we use the current URL as a starting
+  // point to retain URI parts not supported by the modal. The unsupported
+  // parts may originate from a connect alias or by manual edit of the URL
+  // field.
+  var current = getConnectURL()
+  let url = URI(current)
+    .protocol($('#transportSelect').val())
+    .hostname($('#addrInput').val())
+    .path($('#targetInput').val());
   if ($('#freqInput').val() && $('#freqInput').parent().hasClass('has-success')) {
-    params += '&freq=' + $('#freqInput').val();
+    url = url.setQuery("freq", $('#freqInput').val());
+  } else {
+    url = url.removeQuery("freq");
   }
   if ($('#bandwidthInput').val()) {
-    params += '&bw=' + $('#bandwidthInput').val();
+    url = url.setQuery("bw", $('#bandwidthInput').val());
+  } else {
+    url = url.removeQuery("bw");
   }
   if ($('#radioOnlyInput').is(':checked')) {
-    params += '&radio_only=true';
+    url = url.setQuery("radio_only", "true");
+  } else {
+    url = url.removeQuery("radio_only");
   }
-
-  if (params) {
-    url += params.replace('&', '?');
-  }
-
-  return url;
+  return url.build();
 }
 
 function onConnectFreqChange() {

commit 76d9fd57d206632a66f1f048e82ac90f2da87d9b

Fix build of telnet:// URIs in web gui
Bug introduced in 7d43a6d534955807ed8dc243998b40f6acd6ffb2.

web/src/js/app.js
@@ -685,11 +685,16 @@ function buildConnectURL() {
   // point to retain URI parts not supported by the modal. The unsupported
   // parts may originate from a connect alias or by manual edit of the URL
   // field.
-  var current = getConnectURL()
-  let url = URI(current)
-    .protocol($('#transportSelect').val())
-    .hostname($('#addrInput').val())
-    .path($('#targetInput').val());
+  let transport = $('#transportSelect').val();
+  var current = URI(getConnectURL());
+  var url;
+  if (transport === 'telnet') {
+    // Telnet is special cased, as the address contains more than hostname.
+    url = URI(transport + "://" + $('#addrInput').val() + current.search());
+  } else {
+    url = current.protocol(transport).hostname($('#addrInput').val());
+  }
+  url = url.path($('#targetInput').val());
   if ($('#freqInput').val() && $('#freqInput').parent().hasClass('has-success')) {
     url = url.setQuery("freq", $('#freqInput').val());
   } else {